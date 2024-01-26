16f8aed51d39bd664bed7db46fbe65f23edd0b11
Artem <maksaretem1347@ya.ru>
2024-01-26 12:57:15 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import React from "react";
+import {createRoot} from "react-dom";
+
+import Counter, { randNumber } from "./utils/functions";
+
+
+
+const n = new Counter();
+n.changeVal(randNumber(101))
+console.log(n.val)
+n.inc()
+n.inc()
+
+const tag = createRoot(document.getElementById("root"));
+
+tag.render(<h1>{n.val}</h1>)
+
---
+++ src/utils/functions.js
@@ -0,0 +1,26 @@
+export const randNumber = function(max = 100, min = 0) {
+  return Math.floor(Math.random() * (max - min) + min );
+}
+export const getRGB = () => {
+  return `rgb(${randNumber(256)},${randNumber(256)},${randNumber(256)})`
+}
+
+
+class Counter {
+  constructor() {
+    this.val = 0;
+  }
+  changeVal(newVal) {
+    this.val = newVal;
+  }
+  inc() {
+    this.val += 1;
+  }
+  dec() {
+    this.val -= 1;
+  }
+}
+
+export default Counter;
+
+
